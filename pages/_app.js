e0b1fd695e61b54a615930737ae701b8d79d4ce7
Morgan Page <me@morganpage.co.uk>
2020-12-28 22:22:35 +0000

    Facebook pixel added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import FacebookPixel from "../components/FacebookPixel";
 
 const fontFamily = process.env.NEXT_PUBLIC_FONT_FAMILY || "'Nunito Sans', sans-serif";
 
@@ -20,7 +21,7 @@ const theme = extendTheme({
           fontSize: "5xl",
           fontWeight: "bold",
           padding: "12px 0 12px 0",
-          lineHeight: "1.3em"
+          lineHeight: "1.3em",
         },
         h2: {
           fontSize: "2xl",
@@ -39,38 +40,37 @@ const theme = extendTheme({
         },
         p: {
           margin: "0px 0 12px 0",
-          fontSize:"24px",
-          lineHeight: "normal"
+          fontSize: "24px",
+          lineHeight: "normal",
         },
         ul: {
           padding: "0px 0 8px 28px",
-          fontSize:"20px"
-        }
+          fontSize: "20px",
+        },
       },
       ".box": {
         borderRadius: "0px",
         border: "2px solid #333",
         padding: "10px",
       },
-      hr:{
-        margin:"22px 0"
+      hr: {
+        margin: "22px 0",
       },
       ".testimonial": {
         p: {
-          fontSize:"2xl"
-        }
-      }
+          fontSize: "2xl",
+        },
+      },
     },
   },
 });
 
-
-
-
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <FacebookPixel>
+        <Component {...pageProps} />
+      </FacebookPixel>
     </ChakraProvider>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { FB_PIXEL_ID } from '../lib/fpixel'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -7,14 +8,14 @@ class MyDocument extends Document {
   }
 
   render() {
-    let fontLink = process.env.NEXT_PUBLIC_FONT_LINK ||"https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700;900&display=swap";
+    let fontLink =
+      process.env.NEXT_PUBLIC_FONT_LINK || "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700;900&display=swap";
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href={fontLink} rel="stylesheet" />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href={fontLink} rel="stylesheet"/>
-          
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}></script>
 
           <script
@@ -24,12 +25,34 @@ class MyDocument extends Document {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${process.env.GA_MEASUREMENT_ID}');
-              `
+              `,
             }}
           />
 
-
-
+          {/* Global Site Code Pixel - Facebook Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${FB_PIXEL_ID});
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
