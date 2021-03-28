fd6b49e5f00a59519dbcc9e99b46683e4ac683a8
Morgan Page <morganjpage@gmail.com>
2021-03-28 23:42:58 +0100

    Blog added to nav

--- components/nav.js
+++ components/nav.js
@@ -1,15 +1,15 @@
-import { Flex,Box } from "@chakra-ui/react";
+import { Flex,Box,Text } from "@chakra-ui/react";
 import Link from "next/link";
 import Image from "./image";
 
 const Nav = ({global}) => {
   return (
-    <Flex alignItems="center" justifyContent="space-between" maxWidth={1280} mx="auto">
-      <Box w={200}>
+    <Flex alignItems="center" justifyContent="space-between" maxWidth={1100} mx="auto" px={8}>
+      <Box w={200} >
         <Link href="/"><a><Image image={global.logo}/></a></Link>
       </Box>
-      <Box w={200}>
-        
+      <Box w={200} >
+      <a href="https://blog.morganpage.tech"><Text fontWeight="bold" fontSize="1.3em" color={"gray.600"}  textAlign="right">BLOG</Text></a>
       </Box>
     </Flex>
   )
